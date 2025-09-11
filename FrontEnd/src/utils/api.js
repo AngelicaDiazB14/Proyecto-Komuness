8cae8ad769c1720625882e1968d79994928e2601
AngelicaDiazB14 <diazbarriosangelica00@gmail.com>
2025-09-11 15:38:43 -0600

    SCRUM-7: Frontend - Utilidad API categorías

--- FrontEnd/src/utils/api.js
+++ FrontEnd/src/utils/api.js
@@ -1,1 +1,15 @@
 export const API_URL = "http://159.54.148.238/api";
+
+
+export const getCategoriaById = async (id) => {
+  try {
+    const response = await fetch(`${API_URL}/categorias/${id}`);
+    if (response.ok) {
+      return await response.json();
+    }
+    return null;
+  } catch (error) {
+    console.error('Error al obtener categoría:', error);
+    return null;
+  }
+};
