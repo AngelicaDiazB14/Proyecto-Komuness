e7d388d7ff51b2e1dab9606263327adf888bc1ed
AngelicaDiazB14 <diazbarriosangelica00@gmail.com>
2025-09-11 15:37:36 -0600

    SCRUM-7: Frontend - Categoría en detalle publicación

--- FrontEnd/src/components/publicacionDetalle.js
+++ FrontEnd/src/components/publicacionDetalle.js
@@ -7,6 +7,7 @@ import Slider from "./slider";
 import ComentariosPub from "./comentariosPub";
 import PublicacionModal from "./publicacionModal";
 import { useAuth } from "./context/AuthContext";
+import CategoriaBadge from './categoriaBadge';
 
 export const PublicacionDetalle = () => {
   const navigate = useNavigate();
@@ -80,6 +81,10 @@ export const PublicacionDetalle = () => {
 
   return (
     <div className="min-h-screen bg-gray-800/80">
+      <div className="flex items-center gap-2 mb-4">
+        <strong>Clasificación:</strong>
+        <CategoriaBadge categoria={publicacion.categoria} />
+      </div>
       <div className="max-w-4xl mx-auto p-4 space-y-6">
         <div className="md:hidden flex justify-between w-full mb-4">
           <button
@@ -115,7 +120,7 @@ export const PublicacionDetalle = () => {
                 {user && (user.tipoUsuario === 0 || user.tipoUsuario === 1) && (
                   <div>
                     <button
-                      className="bg-red-500 py-2 px-4 rounded hover:bg-red-600"
+                      className="bg-red-600 py-2 px-4 rounded hover:bg-red-600"
                       onClick={() => setSelectedPub(true)}
                     >
                       Eliminar
